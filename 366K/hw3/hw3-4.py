0ffbc7bdd56c04636482b7dfec502aeb02f8c265
Gilbert Martinez <gilbert.martinez675@utexas.edu>
2019-09-17 20:17:29 -0500

    number 4

--- 366K/hw3/hw3-4.py
+++ 366K/hw3/hw3-4.py
@@ -1,3 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+theta = np.linspace(0, 2*np.pi, 361)  # all the radians
+e = np.linspace(0, .8, 5)
+for i in range(np.size(e)):
+    e_temp = e[i]
+    r = 10000*np.divide(1, (1+np.multiply(e_temp, np.cos(theta))))
+    x = np.multiply(r, np.cos(theta))
+    y = np.multiply(r, np.sin(theta))
+    plt.plot(x, y)
+plt.show()
