3101379699958a7bf689f1288d150c094b55ded0
Gilbert Martinez <gilbert.martinez675@utexas.edu>
2019-09-18 23:09:01 -0500

    number 4

--- 366K/hw3/hw3-4.py
+++ 366K/hw3/hw3-4.py
@@ -1,12 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+R_P = 10000
+
 theta = np.linspace(0, 2*np.pi, 361)  # all the radians
-e = np.linspace(0, .8, 5)
+e = [0, .2, .4, .6, .8]
 for i in range(np.size(e)):
     e_temp = e[i]
-    r = 10000*np.divide(1, (1+np.multiply(e_temp, np.cos(theta))))
-    x = np.multiply(r, np.cos(theta))
-    y = np.multiply(r, np.sin(theta))
-    plt.plot(x, y)
+    # find radius
+    r = R_P*np.divide(1, (1+np.multiply(e_temp, np.cos(theta))))
+    r = np.divide(r, R_P)
+    # convert from polar to cartesian
+    x = np.multiply(r, np.cos(theta))  # x = rcos(theta)
+    y = np.multiply(r, np.sin(theta))  # y = rsin(theta)
+    plt.plot(x, y, label=str(e_temp))
+plt.grid()
+plt.plot(0, 0, 'bo')
+plt.legend(bbox_to_anchor=(1.2, 1), loc="upper right", borderaxespad=0.)
+plt.title("Orbits with Varying Eccentricity")
+plt.axis('equal')
 plt.show()
--- 366K/hw3/hw3-7.py
+++ 366K/hw3/hw3-7.py
@@ -12,12 +12,10 @@ theta = np.linspace(0, 2*np.pi, 361)  # numpy uses radians
 h = np.cross(r, v)  # momentum
 e = np.cross(v, h)/MU - np.divide(r, lg.norm(r))  # eccentricity
 e_norm = lg.norm(e)
-r = lg.norm(r)
-v = lg.norm(v)
 cos = np.cos(theta)
 h = lg.norm(h)
 # calculate r and v as a function of the anomaly
-r = np.multiply((h*h/MU), np.divide(1, (1+np.multiply(e_norm, cos))))  # r as a func of theta
+r = np.multiply((h*h/MU), np.divide(1, (1+np.multiply(e_norm, cos))))  # orbit equation
 a = (r[205]*(1+e_norm*np.cos(theta[205])))/(1-e_norm*e_norm)  # use any r and theta paring to find a
 v = np.sqrt(MU*(np.divide(2, r))-1/a)  # use r and a to find v
 # Find the energies
