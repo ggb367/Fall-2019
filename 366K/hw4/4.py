1d5cfe9ede0b23ff8f81d7c285f73acbccdff27d
Gilbert Martinez <gilbert.martinez675@utexas.edu>
2019-09-22 17:27:06 -0500

    3 and 4 done

--- 366K/hw4/4.py
+++ 366K/hw4/4.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 # constants
 MU = 398600.4415  # DU^3/TU^2
 
-def orbit_prop(time_series, n, e, t_p):
+def hyper_orbit_prop(time_series, n, e, t_p):
     # allocate memory for anomalies
-    E = np.empty(np.size(time_series))
+    F = np.empty(np.size(time_series))
     M = np.empty(np.size(time_series))
     θ = np.empty(np.size(time_series))
     e_norm = lg.norm(e)
     # Helper Functions
     def f(x, m_e):
-        return x - e_norm * np.sin(x) - m_e
+        return -x+e_norm*np.sinh(x)-m_e
     def df(x):
-        return 1 - e_norm * np.cos(x)
+        return -1+e_norm*np.cosh(x)
     # propagate through the time series
     for i in range(np.size(time_series)):
         M[i] = n*(time_series[i]-t_p)  # mean anomaly for this time step
@@ -27,16 +27,16 @@ def orbit_prop(time_series, n, e, t_p):
         it = 0
         error = 100.0
         while error > 10**-10 and it <= 50:  # newton raphson to find eccentric anomaly
-            E[i] = guess-f(guess, M[i])/df(guess)
-            error = np.abs((E[i]-guess)/E[i])
-            guess = E[i]
+            F[i] = guess-f(guess, M[i])/df(guess)
+            error = np.abs((F[i]-guess)/F[i])
+            guess = F[i]
             it = it+1
-        θ[i] = 2*math.atan2(np.sqrt(1+e_norm)*np.tan(E[i]/2), np.sqrt(1-e_norm))  # find anomaly from eccentric anomaly
-    return θ, E, M
+        θ[i] = 2*math.atan2(np.sqrt(e_norm+1)*np.tanh(F[i]/2), np.sqrt(e_norm-1))  # find anomaly from eccentric anomaly
+    return θ, F, M
 
 # Initial conditions
-r_0 = [-17310, 39, -784]  # km
-v_0 = [-.251, -2.827, 3.754]  # km/s
+r_0 = [0, 39991, -52979]  # km
+v_0 = [0, 0, 3.5511]  # km/s
 
 # a
 r_0_norm = lg.norm(r_0)
@@ -62,25 +62,25 @@ h_norm = lg.norm(h)
 k = (h_norm*h_norm)/(r_0_norm*MU) - 1
 theta_0 = np.arccos(k/e_norm)  # anomaly at initial condition
 if np.dot(r_0, v_0) < 0:
-    theta_0 = 2*np.pi() - theta_0
+    theta_0 = 2*math.pi - theta_0
 print("b. Anomaly at t_0 is "+str(theta_0))
-E_0 = 2*math.atan2(np.sqrt(1-e_norm)*np.tan(theta_0/2), np.sqrt((1+e_norm)))
-print("Eccentric Anomaly at t_0 is: "+str(E_0))
-M_E_0 = E_0-e_norm*np.sin(E_0)
-print("Mean Anomaly at t_0 is: "+str(M_E_0))
+F_0 = 2*math.atan2(np.sqrt(e_norm+1)*np.tanh(theta_0/2), np.sqrt(e_norm-1))
+print("Eccentric Anomaly at t_0 is: "+str(F_0))
+M_h_0 = -F_0+e_norm*np.sinh(F_0)
+print("Mean Anomaly at t_0 is: "+str(M_h_0))
 
 # c
-a = (r_0_norm*(1+e_norm*np.cos(theta_0)))/(1-e_norm*e_norm)
+a = h_norm**2/MU*(e_norm**2-1)**-1
 n = np.sqrt(MU/math.pow(a, 3))
-t_p = M_E_0/n
+t_p = M_h_0/n
 print("c. t_p is: "+str(t_p))
 
 # d
-t = np.linspace(0, 40000, 81)
+t = np.arange(-20000, 20000, 500)
 t_hour = np.divide(t, 3600)
-theta, E, M = orbit_prop(t, n, e, t_p)
-print("Anomaly at t=10000 is: "+str(theta[20]))
-r = np.multiply((h_norm**2/MU), np.divide(1, (1+np.multiply(e_norm, np.cos(theta)))))  # orbit equation
+theta, E, M = hyper_orbit_prop(t, n, e, t_p)
+print("Anomaly at t=10000 is: "+str(theta[60]))
+r = np.multiply((h_norm*h_norm/MU), np.divide(1, (1+np.multiply(e_norm, np.cos(theta)))))  # orbit equation
 a = (r[8]*(1+e_norm*np.cos(theta[8])))/(1-e_norm*e_norm)  # use any r and theta paring to find a
 v = np.sqrt(MU*(np.divide(2, r))-1/a)  # use r and a to find v, from vis-viva
 KE = 0.5*np.square(v)
